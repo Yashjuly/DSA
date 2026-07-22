e30231797531d3e30bb2e81fef43bddc8d9b42dc
Yash <155655482+Yashjuly@users.noreply.github.com>
2026-07-23 00:02:58 +0530

    Time: 3 ms (55.29%) | Memory: 46.3 MB (72.16%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,44 +1,36 @@
-import java.util.HashSet;
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-
-        HashSet<Character>[] row_set = new HashSet[9];
         HashSet<Character>[] col_set = new HashSet[9];
-        HashSet<Character>[] mat_set = new HashSet[9];
+        HashSet<Character>[] row_set = new HashSet[9];
+        HashSet<Character>[] box_set = new HashSet[9];
 
-        // Initialize all sets
-        for (int i = 0; i < 9; i++) {
-            row_set[i] = new HashSet<>();
-            col_set[i] = new HashSet<>();
-            mat_set[i] = new HashSet<>();
+        for(int k=0;k<9;k++){
+            row_set[k] = new HashSet<>();        
+            col_set[k] = new HashSet<>();
+            box_set[k] = new HashSet<>();
         }
 
-        // Traverse the board
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-
-                if (board[row][col] == '.') {
+        for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+                if(board[row][col]=='.'){
                     continue;
                 }
 
                 char num = board[row][col];
+                int boxi =(row/3)*3 +(col/3);
 
-                // Find the 3x3 box number
-                int box = (row / 3) * 3 + (col / 3);
-
-                if (row_set[row].contains(num) ||
-                    col_set[col].contains(num) ||
-                    mat_set[box].contains(num)) {
+                if(row_set[row].contains(num)||
+                   col_set[col].contains(num)||
+                   box_set[boxi].contains(num) ){
                     return false;
                 }
 
                 row_set[row].add(num);
                 col_set[col].add(num);
-                mat_set[box].add(num);
+                box_set[boxi].add(num); 
+
             }
         }
-
         return true;
     }
 }
