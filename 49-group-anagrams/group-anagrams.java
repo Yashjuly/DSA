4f353a77c5783e2f810ad7a063dfd20c793f462e
Yash <155655482+Yashjuly@users.noreply.github.com>
2026-07-24 00:42:22 +0530

    Time: 7 ms (73.75%) | Memory: 49.5 MB (72.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,6 +1,6 @@
 class Solution {
    public List<List<String>> groupAnagrams(String[] strs) {
-    Map<String, List<String>> map = new HashMap<>();
+    HashMap<String, List<String>> map = new HashMap<>();
 
     for(String s : strs){
         char[] arr = s.toCharArray();
